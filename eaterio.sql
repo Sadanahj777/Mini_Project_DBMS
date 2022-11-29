cae496cdf22bd4e9a7d60b91c57e8e7d1b9465a4
Sadanahj777 <2020cs_sadanahj_b@nie.ac.in>
2022-11-29 01:54:07 -0600

    add

---
+++ eaterio.sql
@@ -0,0 +1,371 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 29, 2022 at 08:02 AM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 7.4.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `eaterio`
+--
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `customer_order` (IN `order_id` INT(11))   BEGIN
+	SELECT orh.orh_refcode AS reference_code, CONCAT(c.c_firstname,' ',c.c_lastname) AS customer_name, s.s_name AS shop_name,f.f_name AS food_name,ord.ord_buyprice AS buy_price, ord.ord_amount AS amount ,ord.ord_note AS order_note, orh.orh_ordertime AS order_time , orh.orh_pickuptime AS pickup_time
+    FROM order_header orh 
+    INNER JOIN order_detail ord ON orh.orh_id = ord.orh_id
+    INNER JOIN food f ON f.f_id = ord.f_id
+    INNER JOIN customer c ON orh.c_id = c.c_id
+    INNER JOIN shop s ON orh.s_id = s.s_id
+    WHERE orh.orh_id = order_id; 
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `customer_order_history` (IN `customer_id` INT(11))   BEGIN
+	SELECT orh.orh_refcode AS reference_code, CONCAT(c.c_firstname,' ',c.c_lastname) AS customer_name,
+    s.s_name AS shop_name, orh.orh_ordertime AS order_time, orh.orh_pickuptime AS pickup_time,
+    p.p_amount AS order_cost, orh.orh_orderstatus AS order_status
+    FROM order_header orh INNER JOIN customer c ON orh.c_id = c.c_id
+    INNER JOIN payment p ON orh.p_id = p.p_id
+    INNER JOIN shop s ON orh.s_id = s.s_id
+    WHERE c.c_id = customer_id;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `shop_alltime_revenue` (IN `shop_id` INT(11))   BEGIN
+	SELECT SUM(ord.ord_amount*ord.ord_buyprice) AS alltime_revenue 
+    FROM order_header orh INNER JOIN order_detail ord ON orh.orh_id = ord.orh_id
+    INNER JOIN food f ON f.f_id = ord.f_id INNER JOIN shop s ON s.s_id = orh.s_id
+    WHERE s.s_id = shop_id AND orh.orh_orderstatus = 'FNSH';
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `shop_menu_revenue` (IN `shop_id` INT(11))   BEGIN
+	SELECT f.f_name AS food_name, SUM(ord.ord_amount*ord.ord_buyprice) AS menu_revenue
+    FROM order_header orh INNER JOIN order_detail ord ON orh.orh_id = ord.orh_id
+    INNER JOIN food f ON f.f_id = ord.f_id
+    WHERE orh.s_id = shop_id AND orh.orh_orderstatus = 'FNSH'
+    GROUP BY ord.f_id ORDER BY menu_revenue DESC;
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `ct_id` int(11) NOT NULL,
+  `c_id` int(11) NOT NULL,
+  `s_id` int(11) NOT NULL,
+  `f_id` int(11) NOT NULL,
+  `ct_amount` int(11) NOT NULL,
+  `ct_note` text COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `cart`
+--
+
+INSERT INTO `cart` (`ct_id`, `c_id`, `s_id`, `f_id`, `ct_amount`, `ct_note`) VALUES
+(64, 21, 102, 35, 1, ''),
+(65, 21, 102, 36, 1, '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `c_id` int(11) NOT NULL,
+  `c_username` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `c_pwd` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `c_firstname` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `c_lastname` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `c_email` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `c_gender` varchar(1) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'M for Male, F for Female',
+  `c_type` varchar(3) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Type of customer in this canteen (STD for student, INS for instructor, STF for staff, GUE for guest, ADM for admin, OTH for other)'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`c_id`, `c_username`, `c_pwd`, `c_firstname`, `c_lastname`, `c_email`, `c_gender`, `c_type`) VALUES
+(21, 'pujitha', 'pujitharc', 'Pujitha', 'RC', 'pujitharoopa@gmail.com', 'F', 'STD');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `food`
+--
+
+CREATE TABLE `food` (
+  `f_id` int(11) NOT NULL,
+  `s_id` int(11) NOT NULL,
+  `f_name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `f_price` decimal(6,2) NOT NULL,
+  `f_todayavail` tinyint(4) NOT NULL DEFAULT 1 COMMENT 'Food is available to order or not',
+  `f_preorderavail` tinyint(4) NOT NULL DEFAULT 1,
+  `f_pic` text COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `food`
+--
+
+INSERT INTO `food` (`f_id`, `s_id`, `f_name`, `f_price`, `f_todayavail`, `f_preorderavail`, `f_pic`) VALUES
+(11, 101, 'Masala Dosa', '40.00', 0, 1, 'mdosa.jpg'),
+(12, 101, 'Plain Dosa', '30.00', 1, 1, 'pdosa.jpg'),
+(13, 101, 'Idli', '30.00', 1, 1, 'idli.jpg'),
+(14, 101, 'Vade', '10.00', 1, 1, 'vade.jpg'),
+(15, 101, 'Palav', '30.00', 1, 1, 'palav.jpeg'),
+(16, 101, 'Chapati', '30.00', 1, 1, 'chapati.jpg'),
+(17, 101, 'Parota', '30.00', 1, 1, 'parota.jpg'),
+(18, 101, 'Noodles', '40.00', 1, 1, 'noodels.jpg'),
+(19, 101, 'Gobi', '35.00', 1, 1, 'gobi.jpeg'),
+(20, 101, 'Fried Rice', '40.00', 1, 1, 'frice.jpg'),
+(35, 102, 'Veg Sandwich', '40.00', 1, 1, '35_102.webp'),
+(36, 102, 'Corn Sandwich', '40.00', 1, 1, '36_102.jpg'),
+(37, 102, 'Maggi', '30.00', 1, 1, '37_102.jpg'),
+(38, 102, 'Cold coffee', '20.00', 1, 1, '38_102.webp');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_detail`
+--
+
+CREATE TABLE `order_detail` (
+  `ord_id` int(11) NOT NULL,
+  `orh_id` int(11) NOT NULL,
+  `f_id` int(11) NOT NULL,
+  `ord_amount` int(11) NOT NULL,
+  `ord_buyprice` decimal(6,2) NOT NULL COMMENT 'To keep the snapshot of selected menu cost at the time of the purchase.',
+  `ord_note` text COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_header`
+--
+
+CREATE TABLE `order_header` (
+  `orh_id` int(11) NOT NULL,
+  `orh_refcode` varchar(15) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `c_id` int(11) NOT NULL,
+  `s_id` int(11) NOT NULL,
+  `p_id` int(11) NOT NULL,
+  `orh_ordertime` timestamp NOT NULL DEFAULT current_timestamp(),
+  `orh_pickuptime` datetime NOT NULL,
+  `orh_orderstatus` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `orh_finishedtime` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE `payment` (
+  `p_id` int(11) NOT NULL,
+  `c_id` int(11) NOT NULL,
+  `p_type` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `p_amount` decimal(7,2) NOT NULL,
+  `p_detail` text COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shop`
+--
+
+CREATE TABLE `shop` (
+  `s_id` int(11) NOT NULL,
+  `s_username` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `s_pwd` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `s_name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `s_location` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `s_openhour` time NOT NULL,
+  `s_closehour` time NOT NULL,
+  `s_status` tinyint(4) NOT NULL DEFAULT 1 COMMENT 'Shop ready for taking an order or not (True for open, False for close)',
+  `s_preorderStatus` tinyint(4) NOT NULL DEFAULT 1 COMMENT 'Shop is ready for tomorrow pre-order or not',
+  `s_email` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `s_phoneno` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `s_pic` text COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `shop`
+--
+
+INSERT INTO `shop` (`s_id`, `s_username`, `s_pwd`, `s_name`, `s_location`, `s_openhour`, `s_closehour`, `s_status`, `s_preorderStatus`, `s_email`, `s_phoneno`, `s_pic`) VALUES
+(101, 'Canteen1', 'canteen1', 'GJB CANTEEN', 'GJB', '10:00:00', '05:00:00', 1, 1, 'canteen1@gmail.com', '9999999999', NULL),
+(102, 'Canteen2', 'canteen2', 'NRN CANTEEN', 'NRN', '10:00:00', '05:00:00', 1, 1, 'canteen2@gmail.com', '8888888888', NULL),
+(103, 'Canteen3', 'canteen3', 'Srikantu Canteen', 'Admin Block', '10:00:00', '05:00:00', 1, 1, 'canteen3@gmail.com', '7777777777', NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`ct_id`),
+  ADD KEY `fk_ct_c_idx` (`c_id`),
+  ADD KEY `fk_ct_s_idx` (`s_id`),
+  ADD KEY `fk_ct_f_idx` (`f_id`);
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`c_id`),
+  ADD UNIQUE KEY `c_username` (`c_username`),
+  ADD UNIQUE KEY `c_email` (`c_email`);
+
+--
+-- Indexes for table `food`
+--
+ALTER TABLE `food`
+  ADD PRIMARY KEY (`f_id`),
+  ADD KEY `food_shop_s_id_idx` (`s_id`);
+
+--
+-- Indexes for table `order_detail`
+--
+ALTER TABLE `order_detail`
+  ADD PRIMARY KEY (`ord_id`),
+  ADD KEY `fk_orh_ord_idx` (`orh_id`),
+  ADD KEY `fk_f_ord_idx` (`f_id`);
+
+--
+-- Indexes for table `order_header`
+--
+ALTER TABLE `order_header`
+  ADD PRIMARY KEY (`orh_id`),
+  ADD KEY `fk_orh_idx` (`c_id`),
+  ADD KEY `fk_s_orh_idx` (`s_id`),
+  ADD KEY `fk_p_orh_idx` (`p_id`);
+
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`p_id`),
+  ADD KEY `p_c_fk_idx` (`c_id`);
+
+--
+-- Indexes for table `shop`
+--
+ALTER TABLE `shop`
+  ADD PRIMARY KEY (`s_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `ct_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=66;
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT for table `food`
+--
+ALTER TABLE `food`
+  MODIFY `f_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+
+--
+-- AUTO_INCREMENT for table `order_detail`
+--
+ALTER TABLE `order_detail`
+  MODIFY `ord_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- AUTO_INCREMENT for table `order_header`
+--
+ALTER TABLE `order_header`
+  MODIFY `orh_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
+-- AUTO_INCREMENT for table `payment`
+--
+ALTER TABLE `payment`
+  MODIFY `p_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT for table `shop`
+--
+ALTER TABLE `shop`
+  MODIFY `s_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=104;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `cart`
+--
+ALTER TABLE `cart`
+  ADD CONSTRAINT `fk_ct_c` FOREIGN KEY (`c_id`) REFERENCES `customer` (`c_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk_ct_f` FOREIGN KEY (`f_id`) REFERENCES `food` (`f_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk_ct_s` FOREIGN KEY (`s_id`) REFERENCES `shop` (`s_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `food`
+--
+ALTER TABLE `food`
+  ADD CONSTRAINT `fk_food_shop_id` FOREIGN KEY (`s_id`) REFERENCES `shop` (`s_id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `order_detail`
+--
+ALTER TABLE `order_detail`
+  ADD CONSTRAINT `fk_f_ord` FOREIGN KEY (`f_id`) REFERENCES `food` (`f_id`),
+  ADD CONSTRAINT `fk_orh_ord` FOREIGN KEY (`orh_id`) REFERENCES `order_header` (`orh_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `order_header`
+--
+ALTER TABLE `order_header`
+  ADD CONSTRAINT `fk_c_orh` FOREIGN KEY (`c_id`) REFERENCES `customer` (`c_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk_p_orh` FOREIGN KEY (`p_id`) REFERENCES `payment` (`p_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk_s_orh` FOREIGN KEY (`s_id`) REFERENCES `shop` (`s_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `payment`
+--
+ALTER TABLE `payment`
+  ADD CONSTRAINT `fk_p_c` FOREIGN KEY (`c_id`) REFERENCES `customer` (`c_id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
